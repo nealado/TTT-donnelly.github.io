df2155dc443f8a0d02f6c56d3bf4a391ced47a57
Victoria Donnelly <vic.n.d1@gmail.com>
2015-08-27 17:32:28 -0400

    added game functionality for multiplayer and victory checks

--- TTT.js
+++ TTT.js
@@ -10,13 +10,13 @@ var game = [
 ];
 
 
+var playerMove = 0;
 
 
 // Function to update the array
 
   function updateArrayBoard(){
 
-    var playerMove = 0;
 
 // <rect id="box-r1c1"
 
@@ -42,11 +42,15 @@ var boxClickedIDToString = (event.currentTarget.id).toString();
 var pos = boxClickedIDToString.replace('r','').split('c');
 
 if(playerMove % 2 == 0) {
-  game[pos[0]][pos[1]] = 1;
   $('line#xPiece-'+event.currentTarget.id).velocity(
   {
     strokeWidth: 10
   });
+  game[pos[0]][pos[1]] = 1;
+
+if(game[pos[0]][pos[1]] !== null) {
+  victoryChecking();
+}
 
 } else {
   game[pos[0]][pos[1]] = 0;
@@ -54,6 +58,8 @@ if(playerMove % 2 == 0) {
   {
     opacity: 1
   });
+  victoryChecking();
+
 }
   playerMove += 1
 
@@ -144,20 +150,65 @@ if(playerMove % 2 == 0) {
 updateArrayBoard();
 
   function victoryChecking(){
+    var winner = ""
 
+    if (playerMove % 2 == 0) {
+      winner = "X "
+    } else {
+      winner = "O "
+    }
     //Defines all possible combinations for victory
 
-    var xVictoryR1 = [1, 1, 1];
-
-    for (var i=0; i<game[0].length; i++){
-      for (var j=0; j<xVictoryR1.length; j++) {
-        if (xVictoryR1[i] === game[0][i]) {
-
-        }
-      }
+    if(game[0][0] == 0 && game[0][1] == 0 && game[0][2] == 0){
+      $("<p>"+winner+"Wins!</p>").appendTo("h1");
+    } else if (game[1][0] == 0 && game[1][1] == 0 && game[1][2] == 0) {
+      $("<p>"+winner+"Wins!</p>").appendTo("h1");
+    } else if (game[2][0] == 0 && game[2][1] == 0 && game[2][2] == 0) {
+      $("<p>"+winner+"Wins!</p>").appendTo("h1");
+    } else if (game[0][0] == 0 && game[1][0] == 0 && game[2][0] == 0) {
+      $("<p>"+winner+"Wins!</p>").appendTo("h1");
+    } else if (game[0][1] == 0 && game[1][1] == 0 && game[2][1] == 0){
+      $("<p>"+winner+"Wins!</p>").appendTo("h1");
+    } else if (game[0][2] == 0 && game[1][2] == 0 && game[2][2] == 0){
+      $("<p>"+winner+"Wins!</p>").appendTo("h1");
+    } else if (game[0][0] == 0 && game[1][1] == 0 && game[2][2] == 0){
+      $("<p>"+winner+"Wins!</p>").appendTo("h1");
+    } else if (game[0][2] == 0 && game[1][1] == 0 && game[2][0] == 0){
+      $("<p>"+winner+"Wins!</p>").appendTo("h1");
     }
-    console.log("X wins!")
+
+    // X Win Possibilities
+    else if(game[0][0] == 1 && game[0][1] == 1 && game[0][2] == 1){
+      $("<p>"+winner+"Wins!</p>").appendTo("h1");
+    } else if (game[1][0] == 1 && game[1][1] == 1 && game[1][2] == 1) {
+     $("<p>"+winner+"Wins!</p>").appendTo("h1");
+   } else if (game[2][0] == 1 && game[2][1] == 1 && game[2][2] == 1) {
+     $("<p>"+winner+"Wins!</p>").appendTo("h1");
+   } else if (game[0][0] == 1 && game[1][0] == 1 && game[2][0] == 1) {
+     $("<p>"+winner+"Wins!</p>").appendTo("h1");
+   } else if (game[0][1] == 1 && game[1][1] == 1 && game[2][1] == 1){
+     $("<p>"+winner+"Wins!</p>").appendTo("h1");
+   } else if (game[0][2] == 1 && game[1][2] == 1 && game[2][2] == 1){
+     $("<p>"+winner+"Wins!</p>").appendTo("h1");
+   } else if (game[0][0] == 1 && game[1][1] == 1 && game[2][2] == 1){
+     $("<p>"+winner+"Wins!</p>").appendTo("h1");
+   } else if (game[0][2] == 1 && game[1][1] == 1 && game[2][0] == 1){
+     $("<p>"+winner+"Wins!</p>").appendTo("h1");
+   }
+
   }
+
+    // var xVictoryR1 = [1, 1, 1];
+    //
+    // for (var i=0; i<game[0].length; i++){
+    //   for (var j=0; j<xVictoryR1.length; j++) {
+    //     if (xVictoryR1[i] === game[0][i]) {
+    //
+    //     }
+    //   }
+    // }
+    // console.log("X wins!")
+
 
 
 
