0c2a2b223ac8f90f4b0d6c629c84eb7f5917cb63
Victoria Donnelly <vic.n.d1@gmail.com>
2015-08-28 12:51:44 -0400

    everything working except how to stop game

--- TTT.js
+++ TTT.js
@@ -1,153 +1,57 @@
 $( document ).ready(function() {
 
-console.log("TTT JS loaded");
+  console.log("TTT JS loaded");
 
+  var game = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  ];
 
-var game = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null]
-];
+  var playerMove = 0;
 
-
-var playerMove = 0;
-
-
-// Function to update the array
+  // Function to update the game board and track pieces in array & on screen.
 
   function updateArrayBoard(){
 
+    // On click
 
-// <rect id="box-r1c1"
+    $( "rect" ).click(function(event) {
 
-    var r1c1SquareArray = game[0][0];
-    var r1c2SquareArray = game[0][1];
-    var r1c3SquareArray = game[0][2];
+      var boxClickedIDToString = (event.currentTarget.id).toString();
+      var pos = boxClickedIDToString.replace('r','').split('c');
 
-    var r2c1SquareArray = game[1][0];
-    var r2c2SquareArray = game[1][1];
-    var r2c3SquareArray = game[1][2];
+      if(playerMove % 2 == 0 && game[pos[0]][pos[1]] == null) {
+        $('line#xPiece-'+event.currentTarget.id).velocity(
+          {
+            strokeWidth: 10
+          });
+        game[pos[0]][pos[1]] = 1;
 
-    var r3c1SquareArray = game[2][0];
-    var r3c2SquareArray = game[2][1];
-    var r3c3SquareArray = game[2][2];
+        if(game[pos[0]][pos[1]] !== null) {
+          victoryChecking();
+        }
 
+      } else if (playerMove % 2 != 0 && game[pos[0]][pos[1]] == null){
+          game[pos[0]][pos[1]] = 0;
+          $('g#oPiece-'+event.currentTarget.id).velocity(
+            {
+              opacity: 1
+            });
+          victoryChecking();
+        } else {
+          alert("Your turn forfeited!");
+        }
+        playerMove += 1
 
-
-// on click...
-
-$( "rect" ).click(function(event) {
-
-var boxClickedIDToString = (event.currentTarget.id).toString();
-var pos = boxClickedIDToString.replace('r','').split('c');
-
-if(playerMove % 2 == 0) {
-  $('line#xPiece-'+event.currentTarget.id).velocity(
-  {
-    strokeWidth: 10
-  });
-  game[pos[0]][pos[1]] = 1;
-
-if(game[pos[0]][pos[1]] !== null) {
-  victoryChecking();
-}
-
-} else {
-  game[pos[0]][pos[1]] = 0;
-  $('g#oPiece-'+event.currentTarget.id).velocity(
-  {
-    opacity: 1
-  });
-  victoryChecking();
-
-}
-  playerMove += 1
-
-  console.log(game);
-
-});
-
-
-// //
-//
-// $( "rect" ).click(function( event ) {
-//   console.log( event.currentTarget.id); // true
-//
-//   var boxClickedOnID = event.currentTarget.id
-//
-//   if(boxClickedOnID == "box-r1c1" && game[0][0] == null) {
-//
-//       if (playerMove % 2 == 0) {
-//         game[0][0] = 1;
-//
-//         $('line#xPiece-r1c1').velocity(
-//         {
-//           strokeWidth: 10
-//         });
-//       }  else {
-//         game[0][0] = 0;
-//           $('g#oPiece-r1c1').velocity(
-//           {
-//             opacity: 1
-//           });
-//         }
-//       }
-//
-//   else if(boxClickedOnID == "box-r1c2" && game[0][1] == null) {
-//
-//     // Count whether or not you'r an x or an O
-//     if (playerMove % 2 == 0) {
-//       $('line#xPiece-r1c2').velocity(
-//       {
-//         strokeWidth: 10
-//       });
-//       game[0][1] = 1;
-//     } else {
-//       $('g#oPiece-r1c2').velocity(
-//       {
-//         opacity: 1
-//       });
-//       game[0][1] = 0;
-//
-//     }
-//
-//   }   else if(boxClickedOnID == "box-r1c3" && game[0][2] == null) {
-//
-//       // Count whether or not you'r an x or an O
-//       if (playerMove % 2 == 0) {
-//         $('line#xPiece-r1c3').velocity(
-//         {
-//           strokeWidth: 10
-//         });
-//         game[0][2] = 1;
-//       } else {
-//         $('g#oPiece-r1c3').velocity(
-//         {
-//           opacity: 1
-//         });
-//         game[0][1] = 0;
-//
-//       }
-//
-//     }
-//
-//   playerMove += 1
-//
-//   console.log("Number of moves played: " + playerMove);
-//   console.log(game[0][0]);
-//   console.log(game[0][1]);
-//   console.log(game[0], game[1], game[2]);
-//
-//   // if(playerMove >= 5) {
-//   //   victoryChecking();
-//   // }
-// console.log(game[0]);
-// });
-
+        console.log(game);
+    });
 
 }
 
 updateArrayBoard();
+
+
 
   function victoryChecking(){
     var winner = ""
@@ -157,7 +61,10 @@ updateArrayBoard();
     } else {
       winner = "O "
     }
+
     //Defines all possible combinations for victory
+
+    //O Win Possibilities
 
     if(game[0][0] == 0 && game[0][1] == 0 && game[0][2] == 0){
       $("<p>"+winner+"Wins!</p>").appendTo("h1");
@@ -196,21 +103,32 @@ updateArrayBoard();
      $("<p>"+winner+"Wins!</p>").appendTo("h1");
    }
 
+   // Stalemate...
+
+   else if(game[0][0] != null && game[0][1] != null && game[0][2] != null
+          && game[1][0] != null && game[1][1] != null && game[1][2] != null
+          && game[2][0] != null && game[2][1] != null && game[2][2] != null
+   ) {
+     $("<p>Stalemate...</p>").appendTo("h1");
+   }
+
+  // console.log(game.every(isAllFilled));
+  //
+  // function isAllFilled(element, index, array) {
+  //
+  //   if (element = null) {
+  //     console.log("Keep playing")
+  //   } else {
+  //     console.log("stop");
+  //   }
+  // }
+
   }
 
-    // var xVictoryR1 = [1, 1, 1];
-    //
-    // for (var i=0; i<game[0].length; i++){
-    //   for (var j=0; j<xVictoryR1.length; j++) {
-    //     if (xVictoryR1[i] === game[0][i]) {
-    //
-    //     }
-    //   }
-    // }
-    // console.log("X wins!")
 
+});
 
-
+// Commented out failed code
 
 // Board R1C1
 //  pos = id.split(-).replace('c'. '').splict('-')
@@ -218,14 +136,29 @@ updateArrayBoard();
 
 // Function get icons to appear
 
+// Old attempt at aligning coordinates
 
-var firstIcon = $('#xPiece-r1c1');
+    // var r1c1SquareArray = game[0][0];
+    // var r1c2SquareArray = game[0][1];
+    // var r1c3SquareArray = game[0][2];
+    //
+    // var r2c1SquareArray = game[1][0];
+    // var r2c2SquareArray = game[1][1];
+    // var r2c3SquareArray = game[1][2];
+    //
+    // var r3c1SquareArray = game[2][0];
+    // var r3c2SquareArray = game[2][1];
+    // var r3c3SquareArray = game[2][2];
 
-// $('rect#box-r1c1').click(function(){
-//   $('g#xPiece-r1c1').velocity(
-//   {
-//     opacity: 1
-//   });
-// });
+// Old attempt at victory function
 
-});
+  // var xVictoryR1 = [1, 1, 1];
+  //
+  // for (var i=0; i<game[0].length; i++){
+  //   for (var j=0; j<xVictoryR1.length; j++) {
+  //     if (xVictoryR1[i] === game[0][i]) {
+  //
+  //     }
+  //   }
+  // }
+  // console.log("X wins!")
